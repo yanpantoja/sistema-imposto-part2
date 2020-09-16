eef431a7d16cf3892e58d6e71c8c1d43cc48c177
Yan Pantoja <yanmpantoja@gmail.com>
2020-09-16 11:10:51 -0300

    feat: data exported with Bridge

---
+++ src/Relatorio/ArquivoZipExportado.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Alura\DesignPattern\Relatorio;
+
+
+class ArquivoZipExportado implements ArquivoExportado
+{
+    private string $nomeArquivoInterno;
+
+    public function __construct(string $nomeArquivoInterno)
+    {
+
+        $this->nomeArquivoInterno = $nomeArquivoInterno;
+    }
+
+    public function exportar(ConteudoExportado $conteudoExportado): string
+    {
+        $caminhoArquivo = tempnam(sys_get_temp_dir(), 'zip');
+        $arquivoZip = new \ZipArchive();
+        $arquivoZip->open($caminhoArquivo);
+        $arquivoZip->addFromString($this->nomeArquivoInterno, serialize($conteudoExportado->conteudo()));
+        $arquivoZip->close();
+
+        return $caminhoArquivo;
+    }
+}
--- src/Relatorio/PedidoExportado.php
+++ src/Relatorio/PedidoExportado.php
@@ -2,7 +2,22 @@
 
 namespace Alura\DesignPattern\Relatorio;
 
+use Alura\DesignPattern\Pedido;
+
 class PedidoExportado implements ConteudoExportado
 {
+    private Pedido $pedido;
 
+    public function __construct(Pedido $pedido)
+    {
+        $this->pedido = $pedido;
+    }
+
+    public function conteudo(): array
+    {
+        return [
+            'data_finalizacao' => $this->pedido->dataFinalizacao->format('d/m/Y'),
+            'nome_cliente' => $this->pedido->nomeCliente
+        ];
+    }
 }
